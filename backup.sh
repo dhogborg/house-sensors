dd8bc64f3e58bc5bc8e7d7e5d65c1ff0742feca1
David Högborg <d@hogborg.se>
2022-09-03 13:23:19 +0200

    backup script

--- backup.sh
+++ backup.sh
@@ -5,6 +5,7 @@ while [ true ]; do
     docker exec -ti house-sensors_influxdb_1 influxd backup -portable -host localhost:8088 /backup/$DATE
     zip -r $DATE ./backup/$DATE
     s3cmd put ./$DATE.zip s3://dhogborg-house-data/
+    rm $DATE.zip
     echo "Sleeping..."
     sleep 86400
 done
