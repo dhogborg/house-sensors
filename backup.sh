4dbc3817ec75f25b7a185733385493f57f719fe7
David Högborg <d@hogborg.se>
2022-09-03 13:23:19 +0200

    new backup script

--- backup.sh
+++ backup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+source tokens.env
+
 while [ true ]; do
     DATE=`date +%F`
     echo "It's $DATE and I'm backing up"
-    docker exec -ti house-sensors_influxdb_1 influxd backup -portable -host localhost:8088 /backup/$DATE
+    docker exec -ti house-sensors_influxdb_1 influx backup -t $INFLUX_TOKEN  /backup/$DATE
     zip -r $DATE ./backup/$DATE
     s3cmd put ./$DATE.zip s3://dhogborg-house-data/
-    rm $DATE.zip
+
     echo "Sleeping..."
     sleep 86400
+    rm $DATE.zip
+    rm -r ./backup/$DATE
 done
